3635ba11395de9a3095ad46e0aa0da72ee6ec955
JeremyZXi <zjeremy0710@gmail.com>
2024-09-20 13:06:16 +0800

    overview add data

--- src/data/catData.js
+++ src/data/catData.js
@@ -20,6 +20,25 @@ export const categoriesData = [
                             '坊，支持生态种植践行者和农民并提高公众对健康饮食的意识。我们特别关注需要\n' +
                             '帮助的群体，帮助他们提升自给自足能力，并通过慈善活动资助教育项目，推动社区的全面健康发展。'
                     },
+                    {
+                        logo: '/path-to-logo1.png',
+                        name: '斯坦福大学医学院亚裔肝脏中心 | ALC',
+                        email: 'partner1@example.com',
+                        description: 'The Asian Liver Center at Stanford University, established by Dr. Samuel So, a professor at Stanford University School of Medicine,\n' +
+                            'is the first non-profit organization in the United States dedicated to the prevention and reatment of hepatitis B specifically for Asians and Asian Americans. Founded in 1996, the center aims to eliminate hepatitis B worldwide through health education and scientific research. It conducts various public welfare activities focused on hepatitis B prevention and has initiated the global Jade Ribbon Campaign to promote hepatitis B prevention and treatment.<br>' +
+                            '斯坦福医学院苏启深教授所建立的美国斯坦福大学\n' +
+                            '亚裔肝脏中心是美国第一个针对亚洲人和亚商的乙肝防治而成立的非营利机构。该中心由斯坦福大学\n' +
+                            '医学院的外科教授苏启深博士成立于1996年，以在全球消除乙型肝炎为目标，健康教育和科学研究\n' +
+                            '为主，开展各种针对乙肝防治的公益活动，并号召\n' +
+                            '并发起了全球的防治乙肝的翡翠丝带项目'
+                    },
+                    {
+                        logo: '/path-to-logo1.png',
+                        name: 'FinTeen',
+                        email: 'partner1@example.com',
+                        description: 'At FinTeens, our mission is to teach and empower fellow high school students everywhere to develop a strong understanding of finance and start shaping their own financial futures. Our emphasis lies in creating engaging educational content that fosters discussions, encourages critical thinking, and nurtures a culture of financial responsibility.<br>'+
+                        'Finteens的目标是向帮助世界各地的高中 生深入了解金融知识、开启他们的金融之 路。Finteens的重心在于开发具有创新性 的教育材料，促进讨论，鼓励批判性思 维，并培养理财能力。'
+                    },
                     // More partners...
                 ]
             },
@@ -29,37 +48,123 @@ export const categoriesData = [
                 items: [
                     {
                         logo: '/path-to-logo2.png',
-                        name: 'Member 1',
-                        email: 'member1@example.com',
-                        description: 'Description for Member 1...',
-                        school: 'School 1'
+                        name: 'Keystone Academy 北京鼎石学校',
+                        email: 'service.council@keystoneacademy.cn',
+                        description: '',
+                        school: ''
                     },
+                    {
+                        logo: '/path-to-logo2.png',
+                        name: 'Beijing Huijia Private School 北京汇佳私立学校',
+                        email: '26huangyuxi@huijia.edu.cn',
+                        description: '',
+                        school: ''
+                    },
+                    {
+                        logo: '/path-to-logo2.png',
+                        name: 'Beijing Haidian Kaiwen Academy 北京海淀凯文学校',
+                        email: 'Student.Council@hd.kaiwenacademy.cn',
+                        description: '',
+                        school: ''
+                    },
+                    {
+                        logo: '/path-to-logo2.png',
+                        name: 'RCF Experimental School A- Level Center 人大附中朝阳分校国际部',
+                        email: 'zhangjunru@rdfzcygj.cn',
+                        description: '',
+                        school: ''
+                    },
+                    {
+                        logo: '/path-to-logo2.png',
+                        name: 'RCF Experimental School A- Level Center 人大附中朝阳分校国际部',
+                        email: 'zhangjunru@rdfzcygj.cn',
+                        description: '',
+                        school: ''
+                    },
+                    {
+                        logo: '/path-to-logo2.png',
+                        name: 'The Experimental High School Attached to Beijing Normal University 北师大附属实验中学国际',
+                        email: 'service.council@keystoneacademy.cn',
+                        description: '',
+                        school: ''
+                    },
+                    {
+                        logo: '/path-to-logo2.png',
+                        name: 'Montverde Academy Shanghai Beijing National Day School 上海梦沃未来学院',
+                        email: 'gloria_wu2007@163.com',
+                        description: '',
+                        school: ''
+                    },
+                    {
+                        logo: '/path-to-logo2.png',
+                        name: 'Beijing National Day School 北京十一学校',
+                        email: 'service.council@keystoneacademy.cn',
+                        description: '',
+                        school: ''
+                    },
+                    {
+                        logo: '/path-to-logo2.png',
+                        name: 'Beijing City International School 北京乐成学校',
+                        email: 'service.council@keystoneacademy.cn',
+                        description: '',
+                        school: ''
+                    },
+                    {
+                        logo: '/path-to-logo2.png',
+                        name: 'Beijing Royal School 北京王府学校',
+                        email: 'service.council@keystoneacademy.cn',
+                        description: '',
+                        school: ''
+                    },
+                    {
+                        logo: '/path-to-logo2.png',
+                        name: 'Aidi International School 北京爱迪国际学校',
+                        email: 'service.council@keystoneacademy.cn',
+                        description: '',
+                        school: ''
+                    },
+                    {
+                        logo: '/path-to-logo2.png',
+                        name: 'Dulwich College Beijing 北京德威英国国际学校',
+                        email: 'service.council@keystoneacademy.cn',
+                        description: '',
+                        school: ''
+                    },
+                    {
+                        logo: '/path-to-logo2.png',
+                        name: '北京中学国际部',
+                        email: 'service.council@keystoneacademy.cn',
+                        description: '',
+                        school: ''
+                    },
+                    {
+                        logo: '/path-to-logo2.png',
+                        name: '八十中国际部',
+                        email: 'service.council@keystoneacademy.cn',
+                        description: '',
+                        school: ''
+                    },
+                    {
+                        logo: '/path-to-logo2.png',
+                        name: 'Xi\'an Foreign Language School 西安外国语国际部',
+                        email: 'service.council@keystoneacademy.cn',
+                        description: '',
+                        school: ''
+                    },
+                    {
+                        logo: '/path-to-logo2.png',
+                        name: 'NLCSS',
+                        email: 'service.council@keystoneacademy.cn',
+                        description: '',
+                        school: ''
+                    },
+
                     // More members...
                 ]
             }
         ]
     },
-    {
-        id: 'cat2',
-        name: 'Category 2',
-        subcategories: [
-            {
-                id: 'partners',
-                name: 'Partners',
-                items: [
-                    // Partners for Category 2...
-                ]
-            },
-            {
-                id: 'members',
-                name: 'Members',
-                items: [
-                    // Members for Category 2...
-                ]
-            }
-        ]
-    },
-    // More main categories...
+
 ];
 
 export default categoriesData
